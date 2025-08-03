d2c35d84ce15a9fd95ce7a1d89035d1a4a0e0833
block <alex.160404@mail.ru>
2025-08-03 14:43:43 +0300

    HW-4: Initial commit

---
+++ src/main/java/ru/chugunov/loggingstartergradle/LoggingStarterAutoConfiguration.java
@@ -0,0 +1,8 @@
+package ru.chugunov.loggingstartergradle;
+
+public class LoggingStarterAutoConfiguration {
+
+    public static void println(String input){
+        System.out.println("Выведено из logging стартера gradle: " + input);
+    }
+}
